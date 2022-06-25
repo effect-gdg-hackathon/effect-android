704fc93af88638e33d4a6561b8275d6135b51701
ParkSungHee <di6223@naver.com>
2022-06-26 03:39:17 +0900

    feat. 마이페이지 - 디자인 추가

---
+++ app/src/main/java/com/github/leeHana21/gdg_hackathon/view/MypageActivity.kt
@@ -0,0 +1,12 @@
+package com.github.leeHana21.gdg_hackathon.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.leeHana21.gdg_hackathon.R
+
+class MypageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage)
+    }
+}
