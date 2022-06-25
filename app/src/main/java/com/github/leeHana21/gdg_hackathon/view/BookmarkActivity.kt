eb0084eb03e4cefa5b5248009b93585ecf674ad6
leeHana21 <leehn0699@gmail.com>
2022-06-26 08:40:21 +0900

    디테일 페이지, 내정보 페이지 작업 및 디자인 수정

--- app/src/main/java/com/github/leeHana21/gdg_hackathon/view/BookmarkActivity.kt
+++ app/src/main/java/com/github/leeHana21/gdg_hackathon/view/BookmarkActivity.kt
@@ -1,26 +1,21 @@
 package com.github.leeHana21.gdg_hackathon.view
 
 import android.os.Bundle
-<<<<<<< HEAD
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.github.leeHana21.gdg_hackathon.R
-import com.github.leeHana21.gdg_hackathon.databinding.FragmentBookmarkBinding
+import com.github.leeHana21.gdg_hackathon.databinding.ActivityBookmarkBinding
 import com.github.leeHana21.gdg_hackathon.entity.Category
 import com.github.leeHana21.gdg_hackathon.entity.PostsData
 
 class BookmarkActivity : AppCompatActivity() {
-    private lateinit var binding: FragmentBookmarkBinding
+    private lateinit var binding: ActivityBookmarkBinding
     private val postAdapter by lazy {
         PostItemsRecyclerViewAdapter(Category.LIFE, values = PostsData.lifeData!!.posts!!)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.fragment_bookmark)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_bookmark)
         binding.uploadRecyclerview.adapter = postAdapter
     }
 }
--- app/src/main/java/com/github/leeHana21/gdg_hackathon/view/MyEffectActivity.kt
+++ app/src/main/java/com/github/leeHana21/gdg_hackathon/view/MyEffectActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.github.leeHana21.gdg_hackathon.R
-import com.github.leeHana21.gdg_hackathon.databinding.FragmentMyeffectBinding
+import com.github.leeHana21.gdg_hackathon.databinding.ActivityMyEffectBinding
 import com.github.leeHana21.gdg_hackathon.entity.Category
 import com.github.leeHana21.gdg_hackathon.entity.PostsData
 
 class MyEffectActivity : AppCompatActivity() {
-    private lateinit var binding: FragmentMyeffectBinding
+    private lateinit var binding: ActivityMyEffectBinding
     private val postAdapter by lazy {
         PostItemsRecyclerViewAdapter(Category.INTERVIEW, values = PostsData.interviewData!!.posts!!)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.fragment_myeffect)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_my_effect)
         binding.myeffectRecyclerview.adapter = postAdapter
     }
 }
